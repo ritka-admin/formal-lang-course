0938b79df45e654467a92bdf11ec72b03b5b7537
ritka_admin <margarita_lazareva30@list.ru>
2023-03-01 20:16:19 +0300

    task2 without tests

---
+++ project/finite_state_automation.py
@@ -0,0 +1,48 @@
+from typing import Any, AbstractSet
+
+import project.graphs
+import networkx as nx
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    EpsilonNFA,
+    State
+)
+
+
+def regex_to_dfa(raw_regex: str) -> DeterministicFiniteAutomaton:
+    regex = Regex(raw_regex)
+    epsilon_nfa = regex.to_epsilon_nfa()
+    deterministic = epsilon_nfa.to_deterministic()
+    return deterministic.minimize()
+
+
+def graph_to_nfa(graph: Any[str, nx.MultiDiGraph],
+                 start_vs: AbstractSet[State] = None,
+                 final_vs: AbstractSet[State] = None) -> EpsilonNFA:
+
+    if type(graph) == str:
+        graph = project.graphs.load_graph(graph_name=graph)
+
+    nfa = EpsilonNFA.from_networkx(graph)
+
+    if start_vs is None:
+        for node in graph.nodes:
+            nfa.add_start_state(node)
+    else:
+        for state in start_vs:
+            nfa.add_start_state(state)
+
+    if final_vs is None:
+        for node in graph.nodes:
+            nfa.add_final_state(node)
+    else:
+        for state in final_vs:
+            nfa.add_start_state(state)
+
+    return nfa
+
+
+
+
+
